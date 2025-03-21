4e021217ce57da9099157f46c81c662c2b516535
bsjhx <rakoczymichal@hotmail.com>
2025-03-21 21:12:54 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    html! { <h1>{ "Hello, Rust Frontend! This is YOLO" }</h1> }
+}
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
