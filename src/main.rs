240407591bde28ebb08c6cdae5c1bf411d6cf567
bsjhx <rakoczymichal@hotmail.com>
2025-03-24 20:30:22 +0100

    Using function Components

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,17 @@ use yew::prelude::*;
 
 #[function_component(App)]
 fn app() -> Html {
-    let squares = squares().iter().map(
-        |s| {
-            if s.is_alive {
-                active_square()
-            } else {
-                square()
-            }
-        }
-    ).collect::<Html>();
-
     html! {
         <div class="grid-container">
-            {squares}
+            <SquaresList squares={squares_generator()} />
         </div>
     }
 }
 
-fn squares() -> Vec<Square> {
+fn squares_generator() -> Vec<Square> {
     let mut result = vec![];
     for i in 0..1950 {
-        if i % 7 == 0 {
+        if i % 50 == 0 {
             result.push(Square { id: i, is_alive: true })
         } else {
             result.push(Square { id: i, is_alive: false })
@@ -34,9 +24,28 @@ fn squares() -> Vec<Square> {
     result
 }
 
+#[derive(Clone, PartialEq)]
 struct Square {
     id: usize,
     is_alive: bool
+}
+
+#[derive(Properties, PartialEq)]
+struct SquareListProps {
+    squares: Vec<Square>,
+}
+
+#[function_component(SquaresList)]
+fn squares_list(SquareListProps { squares }: &SquareListProps) -> Html {
+    squares.iter().map(
+        |s| {
+            if s.is_alive {
+                active_square()
+            } else {
+                square()
+            }
+        }
+    ).collect()
 }
 
 fn square() -> Html {
