527b2063296ad259c2a29f1e03bf6b0bdbc053a8
bsjhx <rakoczymichal@hotmail.com>
2025-03-24 21:09:51 +0100

    Squares are clickable

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,22 @@ use yew::prelude::*;
 
 #[function_component(App)]
 fn app() -> Html {
+    let squares = use_state(|| squares_generator());
+
+    let toggle_square = {
+        let squares = squares.clone();
+        Callback::from(move |clicked_square: Square| {
+            let mut new_squares = (*squares).clone();
+            if let Some(square) = new_squares.iter_mut().find(|sq| sq.id == clicked_square.id) {
+                square.is_alive = !square.is_alive;
+            }
+            squares.set(new_squares);
+        })
+    };
+
     html! {
         <div class="grid-container">
-            <SquaresList squares={squares_generator()} />
+            <SquaresList squares={(*squares).clone()} on_click={toggle_square} />
         </div>
     }
 }
@@ -14,49 +27,56 @@ fn app() -> Html {
 fn squares_generator() -> Vec<Square> {
     let mut result = vec![];
     for i in 0..1950 {
-        if i % 50 == 0 {
-            result.push(Square { id: i, is_alive: true })
-        } else {
-            result.push(Square { id: i, is_alive: false })
-        }
+        result.push(Square {
+            id: i,
+            is_alive: false,
+        });
     }
 
     result
 }
 
-#[derive(Clone, PartialEq)]
+#[derive(Clone, PartialEq, Debug)]
 struct Square {
     id: usize,
-    is_alive: bool
+    is_alive: bool,
 }
 
 #[derive(Properties, PartialEq)]
 struct SquareListProps {
     squares: Vec<Square>,
+    on_click: Callback<Square>,
 }
 
 #[function_component(SquaresList)]
-fn squares_list(SquareListProps { squares }: &SquareListProps) -> Html {
-    squares.iter().map(
-        |s| {
+fn squares_list(SquareListProps { squares, on_click }: &SquareListProps) -> Html {
+    squares
+        .iter()
+        .map(|s| {
+            let on_video_select = {
+                let on_click = on_click.clone();
+                let mut s = s.clone();
+                Callback::from(move |_| on_click.emit(s.clone()))
+            };
+
             if s.is_alive {
-                active_square()
+                active_square(on_video_select)
             } else {
-                square()
+                square(on_video_select)
             }
-        }
-    ).collect()
+        })
+        .collect()
 }
 
-fn square() -> Html {
+fn square(on_video_select: Callback<MouseEvent>) -> Html {
     html! {
-        <div class="square"></div>
+        <div class="square" onclick={on_video_select}></div>
     }
 }
 
-fn active_square() -> Html {
+fn active_square(on_video_select: Callback<MouseEvent>) -> Html {
     html! {
-        <div class="active-square"></div>
+        <div class="active-square" onclick={on_video_select}></div>
     }
 }
 
