6c37fb087842b98b4d61e9a870fad83d9b8ed25a
bsjhx <rakoczymichal@hotmail.com>
2025-03-24 19:50:45 +0100

    Added board view

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,36 @@ use yew::prelude::*;
 
 #[function_component(App)]
 fn app() -> Html {
-    html! { <h1>{ "Hello, Rust Frontend! This is YOLO" }</h1> }
+    html! {
+        <div class="grid-container">
+            {squares()}
+        </div>
+    }
+}
+
+fn squares() -> Vec<Html> {
+    let mut result = vec![];
+    for _i in 0..1950 {
+        if _i > 150 && _i < 1200 {
+            result.push(active_square())
+        } else {
+            result.push(square())
+        }
+    }
+
+    result
+}
+
+fn square() -> Html {
+    html! {
+        <div class="square"></div>
+    }
+}
+
+fn active_square() -> Html {
+    html! {
+        <div class="active-square"></div>
+    }
 }
 
 fn main() {
